d4838e5128a75442bfccb98b3911d45417a6eb07
Fran López <fclopezjurado@gmail.com>
2016-12-26 18:59:50 +0100

    Fixes in 'bootstrap.php' file to active error reporting.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,9 @@
 <?php   // tests/bootstrap.php
-// Settings to make all errors more obvious during testing
-error_reporting(-1);
+
+/**
+ * Settings to make all errors more obvious during testing
+ */
+error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 date_default_timezone_set('UTC');
